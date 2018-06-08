d81dc0df4a8146d9383d352ec252fd6a80b190bb
Ricardo Colombo Ferreira <rdsignbrasil@gmail.com.br>
2018-06-08 15:08:49 -0400

    alteraçõs visuais implementadas + logo e icone na pasta assets

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Alert, View, TouchableOpacity, Text } from 'react-native';
+import { Alert, View, TouchableOpacity, Text, Image } from 'react-native';
 import { KeepAwake } from 'expo';
+import logo from './assets/logo_dark.png';
 
 const corDoBotaoDoGanhador = '#3cba54'
 
@@ -15,23 +16,24 @@ export default class App extends React.Component {
 
   adicionarPonto = (time) => {
     let timePontuado = `pontoTime${time}`
-    let pontoDoTime = this.state[timePontuado]+1
+    let pontoDoTime = this.state[timePontuado] + 1
     this.setState({ [timePontuado]: pontoDoTime })
     this.verificarSeGanhou(pontoDoTime, time)
   }
 
   verificarSeGanhou = (ponto, time) => {
-    if(ponto > 11) this.finalizarOJogo(time)
+    if (ponto > 11) this.finalizarOJogo(time)
   }
 
   finalizarOJogo = (time) => {
-    Alert.alert ( 'Fim de jogo', 'Ganhou!',
-      [ {text: 'OK', onPress: () => { 
+    Alert.alert('Fim de jogo', 'Ganhou!',
+      [{
+        text: 'OK', onPress: () => {
           this.setState({ pontoTime1: 0, pontoTime2: 0 });
           this.reproduzirSomDeVitoria();
         }
       },
-        {text: 'Cancelar', onPress: () => this.removerPonto(time), style: 'cancel'} ],
+      { text: 'Cancelar', onPress: () => this.removerPonto(time), style: 'cancel' }],
       { cancelable: false }
     )
   }
@@ -39,7 +41,7 @@ export default class App extends React.Component {
   removerPonto = (time) => {
     let timePontuado = `pontoTime${time}`
     let pontoDoTime = this.state[timePontuado]
-    if(pontoDoTime > 0) this.setState({ [timePontuado]: pontoDoTime-1 })
+    if (pontoDoTime > 0) this.setState({ [timePontuado]: pontoDoTime - 1 })
   }
 
   reproduzirSomDeVitoria = async () => {
@@ -50,32 +52,43 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={{flex: 1}}>
-        <KeepAwake/>
-        <TouchableOpacity style={this.estiloBotao(this.state.pontoTime1 == 12 ? corDoBotaoDoGanhador : 'powderblue')} onPress={() => this.adicionarPonto(1)} onLongPress={() => this.removerPonto(1)}>
-          <Text style={this.textoBotao()}>{this.state.pontoTime1.toString()}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={this.estiloBotao(this.state.pontoTime2 == 12 ? corDoBotaoDoGanhador : 'skyblue')} onPress={() => this.adicionarPonto(2)} onLongPress={() => this.removerPonto(2)}>
-          <Text style={this.textoBotao()}>{this.state.pontoTime2.toString()}</Text>
-        </TouchableOpacity>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#333333' }}>
+        <KeepAwake />
+        <View style={{ flex: .2, alignItems: 'center', justifyContent: 'center' }}>
+          <Image
+            style={{ flex: .4, marginTop: 10 }}
+            resizeMode='contain'
+            source={logo}
+          />
+        </View>
+        <View style={{ flex: .8, alignItems: 'center', justifyContent: 'flex-start'}} resizeMode='cover'>
+          <TouchableOpacity style={[this.estiloBotao(this.state.pontoTime1 == 12 ? corDoBotaoDoGanhador : 'red'), {borderTopLeftRadius: 50,}]} resizeMode='cover' onPress={() => this.adicionarPonto(1)} onLongPress={() => this.removerPonto(1)}>
+            <Text style={this.textoBotao()}>{this.state.pontoTime1.toString()}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[this.estiloBotao(this.state.pontoTime2 == 12 ? corDoBotaoDoGanhador : 'white'), {borderBottomRightRadius: 50,}]} resizeMode='cover' onPress={() => this.adicionarPonto(2)} onLongPress={() => this.removerPonto(2)}>
+            <Text style={this.textoBotao()}>{this.state.pontoTime2.toString()}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 
   estiloBotao = (cor) => {
     return {
-      flex: 3, 
-      alignItems: 'center', 
-      justifyContent:'center', 
+      flex: 0.47,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 280,
       backgroundColor: cor
     }
   }
 
   textoBotao = () => {
     return {
-      padding: 20,
-      color: 'white',
-      fontSize: 80
+      paddingRight: 0,
+      paddingLeft: 0,
+      color: '#333',
+      fontSize: 120,
     }
   }
 }
